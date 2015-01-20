5cc830db490f23601f3d8806d0a2480f1e4c59d2
danjmercurio <dan.j.mercurio@gmail.com>
2015-01-20 01:23:29 -0800

    style changes

--- board.py
+++ board.py
@@ -1,34 +1,34 @@
-#a python class that represents a board on 8chan
+# a python class that represents a board on 8chan
 class Board():
-	def __init__(self,uri,title,subtitle,time,indexed,sfw,pph,ppd,max,uniq_ip,tags,img,ago):
-		self.uri = uri
-		self.subtitle = subtitle
-		self.img = img
-		self.title = title
-		if (sfw == u'0'):
-			self.sfw = False
-		if (sfw == u'1'):
-			self.sfw = True
-		self.pph = int(pph)
-		self.ppd = int(ppd)
-		self.time = time
-		if (indexed == u'0'):
-			self.indexed = False
-		if (indexed == u'1'):
-			self.indexed = True
-		self.max = max
-		self.ago = ago
-		self.uniq_ip = uniq_ip
-		self.tags = tags
+    def __init__(self, uri, title, subtitle, time, indexed, sfw, pph, ppd, max, uniq_ip, tags, img, ago):
+        self.uri = uri
+        self.subtitle = subtitle
+        self.img = img
+        self.title = title
+        if (sfw == u'0'):
+            self.sfw = False
+        if (sfw == u'1'):
+            self.sfw = True
+        self.pph = int(pph)
+        self.ppd = int(ppd)
+        self.time = time
+        if (indexed == u'0'):
+            self.indexed = False
+        if (indexed == u'1'):
+            self.indexed = True
+        self.max = max
+        self.ago = ago
+        self.uniq_ip = uniq_ip
+        self.tags = tags
 
-	def __repr__(self):
-		return "<8chan /" + self.uri + "/ board " + str(object.__repr__(self))[1:]
+    def __repr__(self):
+        return "<8chan /" + self.uri + "/ board " + str(object.__repr__(self))[1:]
 
-	def isSFW(self):
-		return self.sfw
+    def isSFW(self):
+        return self.sfw
 
-	def isIndexed(self):
-		return self.indexed
+    def isIndexed(self):
+        return self.indexed
 
-	def getThreads(self):
-		return True
+    def getThreads(self):
+        return True
--- main.py
+++ main.py
@@ -13,34 +13,39 @@ MAINURL = 'http://8ch.net/'
 
 # force all connections to be made securely
 def forceSSL():
-	global MAINURL
-	MAINURL = 'https://8ch.net'
+    global MAINURL
+    MAINURL = 'https://8ch.net'
 
 
 # returns a list of Boards
 def getBoards():
-	boards = []
-	boardsJson = json.loads(requests.get(MAINURL + 'boards.json').text)
-	for item in boardsJson:
-		boards.append(Board(item[u'uri'],item[u'title'],item[u'subtitle'],item[u'time'],item[u'indexed'],item[u'sfw'],item[u'pph'],item[u'ppd'],item[u'max'],item[u'uniq_ip'],item[u'tags'],item[u'img'],item[u'ago']))
-	return boards
+    boards = []
+    boardsJson = json.loads(requests.get(MAINURL + 'boards.json').text)
+    for item in boardsJson:
+        boards.append(
+            Board(item[u'uri'], item[u'title'], item[u'subtitle'], item[u'time'], item[u'indexed'], item[u'sfw'],
+                  item[u'pph'], item[u'ppd'], item[u'max'], item[u'uniq_ip'], item[u'tags'], item[u'img'],
+                  item[u'ago']))
+    return boards
 
 
 # returns a Board object if board exists, or False otherwise.
 # don't include slashes in uri argument. for example: 'b', NOT '/b/'
 def getBoard(uri):
-	boards = getBoards()
-	for board in boards:
-		if (board.uri == uri):
-			return board
-	return False
+    boards = getBoards()
+    for board in boards:
+        if (board.uri == uri):
+            return board
+    return False
 
 
 # returns an integer value of the total number of boards on 8chan
 def getNumBoards():
-	boardsNum = 0
-	boardsJson = json.loads(requests.get(MAINURL + 'boards.json').text)
-	for item in boardsJson:
-		boardsNum = boardsNum + 1
-	return boardsNum
-	
+    boardsNum = 0
+    boardsJson = json.loads(requests.get(MAINURL + 'boards.json').text)
+    for item in boardsJson:
+        boardsNum += 1
+    return boardsNum
+
+
+
